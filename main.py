10a9bf6e2610bc74d6e64f40e9834a7851a3dc84
dionis <inoid2007@gmail.com>
2023-02-20 20:56:21 -0500

    New file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from pyabsa import AspectTermExtraction as ATEPC
+#from pyabsa import AspectTermExtraction as ATEPC
 
 
 # now the dataset is a DatasetItem object, which has a name and a list of subdatasets
